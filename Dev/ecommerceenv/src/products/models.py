98aaa4e667097c750ef4bd76f41088e28e908111
Jinyan.LYU <jlyu@wpi.edu>
2018-08-20 10:48:30 -0400

    Add base(with navbar), contact and home_page HTML templates with Django templates.

--- Dev/ecommerceenv/src/products/models.py
+++ Dev/ecommerceenv/src/products/models.py
@@ -2,6 +2,7 @@ import random
 import os
 from django.db import models
 from django.db.models.signals import pre_save, post_save
+from django.urls import reverse
 
 from .utils import unique_slug_generator
 
@@ -51,11 +52,13 @@ class Product(models.Model):
 	image		= models.ImageField(upload_to=upload_image_path, null=True, blank=True)	# upload to MEDIA_ROOT in settings.py
 	featured	= models.BooleanField(default=False)
 	active		= models.BooleanField(default=True)
+	timestamp	= models.DateTimeField(auto_now_add=True)
 
 	objects = ProductManager()	# not overwriting, just extending to it
 
 	def get_absolute_url(self):
-		return f"/products/{self.slug}"
+		# return f"/products/{self.slug}"
+		return reverse('products:detail', kwargs={"slug": self.slug})
 
 	def __str__(self):	# python 3
 		return self.title
